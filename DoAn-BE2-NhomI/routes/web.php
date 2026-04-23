8926a3dd0e3f34df5d27ea266241ce8c0c4662b2
Nguyen Xuan Thu Trang <tn6879065@gmail.com>
2026-04-23 16:42:56 +0700

    them code xu ly login vào  web.php

--- DoAn-BE2-NhomI/routes/web.php
+++ DoAn-BE2-NhomI/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController; 
+use App\Http\Controllers\CrudUserController;
 
 // Khai báo đường dẫn trang chủ 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -9,3 +10,8 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 // ---------------------------------------------------
 // Các route của Trung, Trang, Thực sẽ viết tiếp xuống đây...
 // ---------------------------------------------------
+// hiển thị form login
+Route::get('/login', [CrudUserController::class, 'showLogin']);
+
+// xử lý login khi submit form
+Route::post('/login', [CrudUserController::class, 'login']);
